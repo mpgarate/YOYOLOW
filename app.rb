f34ac5da187c7aea305eaba30e81b688831d1485
Michael Garate <mpgarate@gmail.com>
2014-06-28 10:28:54 -0400

    refactor

--- app.rb
+++ app.rb
@@ -3,18 +3,15 @@ require_relative 'models/yo_user'
 
 Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"))
 
+get '/' do
+  users = YoUser.all.order_by(:yo_count.desc)
+  erb :yo_user_global_highs, locals: {users: users}
+end
+
 get '/yo' do 
   @user = YoUser.find_or_create_by(username: params[:username])
   @user.yo_count += 1 
   @user.save
 
-  names = ""
-  YoUser.all.each do |user|
-    names << user.username
-    names << " | #{user.yo_count}"
-    names << "<br />"  
-  end
-    
-  names
 end
 
