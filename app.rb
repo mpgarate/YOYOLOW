cdac2ddb80419e793d148ebaa1a8ce942f5938d6
Michael Garate <mpgarate@gmail.com>
2014-06-29 11:45:30 -0400

    limit to 10

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require_relative 'models/yo_user'
 Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"))
 
 get '/' do
-  users = YoUser.all.order_by(:yo_count.desc)
+  users = YoUser.all.order_by(:yo_count.desc).take(10)
   erb :yo_user_global_highs, locals: {users: users}, layout: :index
 end
 
@@ -16,7 +16,7 @@ get '/yo' do
   username = username.upcase
  
   @user = YoUser.find_or_create_by(username: username)
-  @user.yo_count += 1 
+  @user.yo_count += 1
   @user.save
 
 end
