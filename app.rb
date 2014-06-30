6a7c1eafff0dc37710b6f0153f2a20ba8c0f847d
root <mpgarate@gmail.com>
2014-06-30 01:15:42 -0400

    minor adjustments

--- app.rb
+++ app.rb
@@ -4,11 +4,15 @@ require_relative 'models/yo_user'
 Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"))
 
 get '/' do
-  users = YoUser.all.order_by(:yo_count.desc).take(10)
+  users = YoUser.all.order_by(:yo_count.desc).take(30)
+
   erb :yo_user_global_highs, locals: {users: users}, layout: :index
 end
 
 get '/yo' do 
+
+  return if request.host == "yoyolowest.mpgarate.com"
+                                    
   username = params[:username]
 
   return if username == nil or username == ""
@@ -18,6 +22,5 @@ get '/yo' do
   @user = YoUser.find_or_create_by(username: username)
   @user.yo_count += 1
   @user.save
-
 end
 
